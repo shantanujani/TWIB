4e5c795c0a8d43c7ef49daf13d78d04f139b3dc4
Shantanu Jani <shantanujani@gmail.com>
2013-05-17 11:14:35 -0500

    TWIB app commit

---
+++ TWIB/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWIB::Application.config.secret_token = 'aeb82b3a0c89e0084869f69164a8ed05343576f612b7a1ae73c27e6cd8a61979ab869aebddf1aa97ea52d0edd332a976e35279b75da6b212e9e2fcfd49898a04'
