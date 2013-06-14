272947e5f57e14c18f8e5d7d41d9bdebcf4f9e3f
Shantanu Jani <shantanujani@gmail.com>
2013-06-13 22:15:22 -0500

    final push

--- TWIB/app/controllers/results_controller.rb
+++ TWIB/app/controllers/results_controller.rb
@@ -2,17 +2,18 @@ class ResultsController < ApplicationController
 
 def results
 
-  @games = User.find_by_id(session["user_id"]).games
+  user = User.find(session["user_id"])
+  @games = user.games
   @wins = 0
   @losses = 0
 
   @games.each do |game|
     if game.home_team_result == "Won"
-       @wins += game.bets.find_by_user_id(session["user_id"]).home_bets_awarded
-       @losses += game.bets.find_by_user_id(session["user_id"]).away_bets_awarded
+       @wins += game.bets.find_by_user_id(user).home_bets_awarded
+       @losses += game.bets.find_by_user_id(user).away_bets_awarded
     elsif game.home_team_result == "Lost"
-       @wins += game.bets.find_by_user_id(session["user_id"]).away_bets_awarded
-       @losses += game.bets.find_by_user_id(session["user_id"]).home_bets_awarded
+       @wins += game.bets.find_by_user_id(user).away_bets_awarded
+       @losses += game.bets.find_by_user_id(user).home_bets_awarded
     end
     # @total = @wins + @losses
     # @ratio = (@wins.to_f/@total)
